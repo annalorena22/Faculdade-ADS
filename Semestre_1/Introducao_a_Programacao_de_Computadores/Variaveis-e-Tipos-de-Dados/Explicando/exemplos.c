306cb7ef4dca872c4e842da994a5ced9b6abc00b
annalorena22 <annalorena1998@gmail.com>
2025-08-18 16:38:51 -0400

    feat(iapdc): tema 2, variáveis e tipos de dados

---
+++ Semestre_1/Introducao_a_Programacao_de_Computadores/Variaveis-e-Tipos-de-Dados/Explicando/exemplos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int idade; // numeros inteiros. ex: 25
+    int quantidade = 1; // numeros inteiros. ex: 10
+    float altura = 1.75; // numeros flutuantes. valores menores. ex: 1.75
+    double peso = 70.5; // numeros reais. valores maiores. ex: 70.5
+    char letra = 'A'; // caracteres. ex: 'A'
+    char nome[20] = "João"; // strings. ex: "João"
+
+    idade = 25; // declarando um valor para idade posteriormente
+
+    quantidade = 10; // alterando o valor declarado anteriormente
+}
